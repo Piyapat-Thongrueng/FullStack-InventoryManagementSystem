f0580cb490cdf6659b593498f7798106c081a1a1
Piyapat <piyapat.thongrueng@gmail.com>
2025-05-05 07:41:43 +0700

    create and configure TransactionService

---
+++ src/main/java/com/InventoryMgtSystem/services/TransactionService.java
@@ -0,0 +1,21 @@
+package com.InventoryMgtSystem.services;
+
+import com.InventoryMgtSystem.dtos.Response;
+import com.InventoryMgtSystem.dtos.TransactionRequest;
+import com.InventoryMgtSystem.enums.TransactionStatus;
+
+public interface TransactionService {
+    Response purchase(TransactionRequest transactionRequest);
+
+    Response sell(TransactionRequest transactionRequest);
+
+    Response returnToSupplier(TransactionRequest transactionRequest);
+
+    Response getAllTransactions(int page, int size, String filter);
+
+    Response getAllTransactionById(Long id);
+
+    Response getAllTransactionByMonthAndYear(int month, int year);
+
+    Response updateTransactionStatus(Long transactionId, TransactionStatus status);
+}
